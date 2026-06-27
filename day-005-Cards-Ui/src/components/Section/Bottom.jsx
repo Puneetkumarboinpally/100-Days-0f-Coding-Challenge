e7a0f0d6aa9ed4939bfe3be66485c51e12fcaf42
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-06-27 17:05:56 +0100

    center section height modified

--- day-005-Cards-Ui/src/components/Section/Bottom.jsx
+++ day-005-Cards-Ui/src/components/Section/Bottom.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const bottom = () => {
-  return <div>bottom</div>;
+const bottom = ({ pay, location }) => {
+  return (
+    <div className="p-1 flex justify-between items:center border-t">
+      <div>
+        <h3>{pay}</h3>
+        <p>{location}</p>
+      </div>
+      <button>Apply now</button>
+    </div>
+  );
 };
 
 export default bottom;
--- day-005-Cards-Ui/src/components/Section/Center.jsx
+++ day-005-Cards-Ui/src/components/Section/Center.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 const Center = ({ company, date, post, tag1, tag2 }) => {
   return (
-    <div>
-      <h2 className="font-bold text-lg">
+    <div className="h-48 mt-6">
+      <h2 className=" font-bold text-lg">
         {company}
-        <span className="px-1 text-xs font-normal text-gray-600 small">{date}</span>
+        <span className="px-1 text-xs font-normal text-gray-600 small">
+          {date}
+        </span>
       </h2>
       <h1 className="font-bold text-2xl mb-2">{post}</h1>
-      <button className="py-1 px-2 bg-gray-200 text-sm mr-2 font-semibold rounded-md">{tag1}</button>
-      <button className="py-1 px-2 bg-gray-200 text-sm font-semibold rounded-md">{tag2}</button>
+      <button className="py-1 px-2 bg-gray-200 text-sm mr-2 font-semibold rounded-md">
+        {tag1}
+      </button>
+      <button className="py-1 px-2 bg-gray-200 text-sm font-semibold rounded-md">
+        {tag2}
+      </button>
     </div>
   );
 };
