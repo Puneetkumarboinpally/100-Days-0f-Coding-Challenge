17b6fd750b3029088af0d19c4d3f5a94370f276a
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-06-27 18:26:57 +0100

    final commit

--- day-005-Cards-Ui/src/components/Section/Top.jsx
+++ day-005-Cards-Ui/src/components/Section/Top.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Bookmark } from "lucide-react";
 
 const Top = ({ logo }) => {
   return (
@@ -7,8 +8,11 @@ const Top = ({ logo }) => {
         src={logo}
         className="w-10 h-10 rounded-full p-1 border border-gray-400 "
       />
-      <div className="border-[1.5px] border-gray-200 py-1 px-2 rounded-md font-bold hover:translate-y-[-2px] transition-transform duration-300">
-        Save <span>💀</span>
+      <div className="border-[1.5px] border-gray-200 py-1 px-2 rounded-md font-bold flex items-center">
+        Save
+        <span className="px-1 text-gray-600">
+          <Bookmark size={16} />
+        </span>
       </div>
     </div>
   );
