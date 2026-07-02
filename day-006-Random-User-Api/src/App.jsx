831b0766d657f9f0dfa24d94c8664878ea0778f4
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-02 21:10:22 +0100

    final commit

--- day-006-Random-User-Api/src/App.jsx
+++ day-006-Random-User-Api/src/App.jsx
@@ -15,8 +15,6 @@ const App = () => {
 
       const data = response.data.results;
       setProfile(data);
-
-      console.log(data);
     } catch (error) {
       setError(error.message);
     } finally {
@@ -29,17 +27,28 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className="p-8 grid sm:grid-cols-3 lg:grid-cols-5 gap-6 bg-gray-600">
       {loading && <p>Loading...</p>}
 
       {error && <p>{error}</p>}
 
       {profile.map((elem, idx) => (
-        <div key={idx}>
-          <img src={elem.picture.medium} alt={elem.name.first} />
+        <div
+          key={idx}
+          className="p-4 bg-white/10 border border-white/20 rounded-lg shadow-md text-center"
+        >
+          <img
+            className="h-48 w-full object-cover"
+            src={elem.picture.medium}
+            alt={elem.name.first}
+          />
           <h2>
-            {elem.name.first} {elem.name.last}
+            {elem.name.first} {elem.name.last}, {elem.dob.age}
           </h2>
+          <p>Username: {elem.login.username}</p>
+          <p>
+            {elem.location.city}, {elem.location.country}
+          </p>
         </div>
       ))}
     </div>
